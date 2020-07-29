6398ccd29e4b6b789b51f31c522b3ae221511340
Ulrich Janus <ulrichjanus@gmail.com>
2020-07-29 08:36:25 +0200

    add dice rolling

--- flask_api.py
+++ flask_api.py
@@ -8,6 +8,7 @@ import json
 from flask import Flask, request, make_response, render_template
 from flask_restful import Resource, Api
 import pandas as pd
+import random
 
 logger = logging.getLogger(__name__)
 
@@ -22,13 +23,38 @@ class HelloWorld(Resource):
         return {"hello": "world"}
 
     def post(self):
-        print("helle post called")
         logger.info(request.form)
-        # return {"hello": "post world"}
-        # import pdb; pdb.set_trace()
-        print(type(request.form))
-        print(request.form)
-        return request.form
+        # payload as keywords
+        # token, team_id, team_domain, channel_id, channel_name, user_id, user_name
+        # command, text, response_url, trigger_id
+        d = requests.form
+
+        # Retrieve command and text
+        command = d["command"]
+        text = d["text"]
+        print(command)
+        print(text)
+        assert(command == "/roll")
+
+        # Text should be of the the form xdy+z
+        # for rolling a y-sided dice x times and adding z
+        # 2d6+3 meansin rolling a six sided dice twice and adding 3 to the sum
+        # Might do this with regular expression, but for now just split
+        try:
+            num_dice = int(text.split("d")[0])
+            num_sides = int(text.split("d")[1].split("+")[0])
+            split_plus = text.split("d")[1].split("+")
+            if len(split_plus) > 1:
+                mod = int(split_plus[1])
+            else:
+                mod = 0
+            dice_result = [ random.randint(1,num_sides) for _ in range(num_dice)]
+            out = f"Rolling {text}: ({'+ '.join(list(map(str, dice_result)))}) + {mod} = {sum(dice_result) + mod}"
+            return out
+        except Exception as e:
+            print(e)
+            return "Something went wrong, try something like: /roll 1d6+2"
+
 
 # Test query string
 class HelloQueryString(Resource):
